48feb1417801bc11c77c36a7960e26ce396042c0
NAGARRO\akshaysrivastava <akshaysrivastava46@gmail.com>
2024-01-31 16:08:51 +0530

    feat: dark mode toggle, link to profile

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,9 +1,38 @@
 import React from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { ModeToggle } from "../mode-toggle";
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card";
 
 type Props = {};
 
 const Header = (props: Props) => {
-  return <div>Header</div>;
+  return (
+    <div className="w-full h-16 flex justify-between items-center px-4 py-2">
+      <h3 className="font-bold text-xl">Weather App</h3>
+      <div className="flex gap-4 items-center">
+        <HoverCard>
+          <HoverCardTrigger
+            className="font-bold underline cursor-pointer"
+            href="https://github.com/akshaypx"
+          >
+            @akshaypx
+          </HoverCardTrigger>
+          <HoverCardContent>
+            Created and maintained by Akshay Srivastava.
+          </HoverCardContent>
+        </HoverCard>
+        <ModeToggle />
+        <Avatar>
+          <AvatarImage src="https://github.com/akshaypx.png" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
