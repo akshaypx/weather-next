5ae0e8eeb8a88d005361ef7a21b6ea3a0f8ff41d
NAGARRO\akshaysrivastava <akshaysrivastava46@gmail.com>
2024-02-01 16:13:26 +0530

    fix: footer links added

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,4 +1,5 @@
 import { Github, Globe, Twitter } from "lucide-react";
+import Link from "next/link";
 import React from "react";
 
 type Props = {};
@@ -8,9 +9,15 @@ const Footer = (props: Props) => {
     <div className="max-w-[900px] w-full p-4 flex gap-4 justify-between items-center">
       <p className="text-muted-foreground">All rights reserved.</p>
       <div className="flex gap-4 justify-center items-center">
-        <Twitter href="https://twitter.com/pxdev0" size={20} />
-        <Github href="https://github.com/akshaypx" size={20} />
-        <Globe href="https://portfolio-2023-drab-nu.vercel.app/" size={20} />
+        <Link href="https://twitter.com/pxdev0">
+          <Twitter size={20} />
+        </Link>
+        <Link href="https://github.com/akshaypx">
+          <Github size={20} />
+        </Link>
+        <Link href="https://portfolio-2023-drab-nu.vercel.app/">
+          <Globe size={20} />
+        </Link>
       </div>
     </div>
   );
